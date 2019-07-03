80efe1a6408192c21871af6a1365b0ea8afa6580
Pierros Ntelis <ge08061@gmail.com>
2019-07-03 16:21:03 +0200

    correct the burnChains compatibility with python 3

--- mypymcLib.py
+++ mypymcLib.py
@@ -471,8 +471,8 @@ def run_mcmc(data,niter=80000, nburn=20000, nthin=1, variables=['Om', 'Ol', 'w']
 def burnChains(chains,kmin=0):
     newChains=dict(chains) # dict(chains)
     # python 2:  kmax = newChains[newChains.keys()[0]].size
-    # python 3: kmax = size(list(newChains.keys()))
-    kmax = size(list(newChains.keys()))
+    # python 3:  kmax = size(newChains[next(iter(newChains))])
+    kmax = size(newChains[next(iter(newChains))])
     for k in newChains.keys(): newChains[k] = newChains[k][kmin:kmax]
     return newChains
 
--- numMath.py
+++ numMath.py
@@ -273,8 +273,8 @@ def merge_chains(totos,nlag=0,KMIN=0):
 def burnChains(chains,kmin=0):
     newChains=dict(chains) # dict(chains)
     # python 2:  kmax = newChains[newChains.keys()[0]].size
-    # python 3: kmax = size(list(newChains.keys()))
-    kmax = size(list(newChains.keys()))
+    # python 3:  kmax = size(newChains[next(iter(newChains))])
+    kmax = size(newChains[next(iter(newChains))])
     for k in newChains.keys(): newChains[k] = newChains[k][kmin:kmax]
     return newChains
 
